941ea63af03468f07c7615671f8e31f035fe92a7
Charles <cgarzon132@gmail.com>
2017-07-01 22:27:20 -0500

    Added some comments to remember what to do with the user auth strategy

--- config/passport.js
+++ config/passport.js
@@ -8,11 +8,19 @@ const config = require("../config/database");
 const user_defaults = "_id username priviledges profile_pic banned is_super alias";
 
 module.exports = (passport) => {
+	// Prepare passport configuration options
 	let opts = {};
-	opts.jwtFromRequest = ExtractJwt.fromAuthHeader(); //Change auth header in prod
+	opts.jwtFromRequest = ExtractJwt.fromAuthHeader(); //Change auth header in prod?
 	opts.secretOrKey = config.secret;
 	opts.algorithms = ["HS512"],
+
+	// Passport auth strategy
 	passport.use(new JwtStrategy(opts,  (jwt_payload, done) => {
+		// Take unencrypted payload and check user last requests
+		// if last request is older than 48 hours or last request modified user profile, reload user info
+		// if token doesnt exist in Redis "Unauthorized"
+		// if user is banned then "Unauthorized"
+		// store active tokens in user to give the user the ability to logout from devices
 		User.findOne({"_id": jwt_payload.iss}, user_defaults, (err, user) => {
 			// This field will contain user and the refreshed token
 			if(err){
